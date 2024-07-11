e205cbe93cdd44cdda1858e36ab4cd0ad9249ad8
arian-k84 <149679341+arian-k84@users.noreply.github.com>
2024-07-11 07:07:51 +0330

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ require_once 'core/model.php';
 ini_set('display_errors', 0);
 // ini_set('display_startup_errors', 0);
 // error_reporting(E_ALL);
-ini_set("log_errors", 1);
-ini_set("error_log", "C:/xampp/php_error.log");
+//ini_set("log_errors", 1);
+//ini_set("error_log", "C:/xampp/php_error.log");
 
 
 new messenger;
